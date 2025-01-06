d22878a5b2ae77a44f2fc962e1fead66be6eabf6
vladikmukin17 <LeykoderTO@gmail.com>
2025-01-06 15:36:05 +0200

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
+import futter from "../../assets/futter.svg"
 import { ShoppingCart, User, Flame, Sun, List } from 'lucide-react';
 
 export const Footer = () => {
   return (
     <>
       {/* Нижнее меню */}
-      <div className="relative bg-[#7A8C65] h-20">
+      <div className="relative bg-[#7A8C65] h-20 mt-10">
         {/* Фоновая "волна" для центральной кнопки */}
-        <div className="absolute -top-8 left-1/2 -translate-x-1/2 w-28 h-16 overflow-hidden">
-          <div className="w-full h-full bg-[#EDF1D6] rounded-b-full" />
-        </div>
+        <img src={futter} className="absolute bg-[#edf1d6] left-1/2 h-full -translate-x-1/2  overflow-hidden">
+          {/*<img src={futter} height="100%"/>*/}
+          {/*<div className="w-full h-full bg-[#EDF1D6] rounded-b-full" />*/}
+        </img>
 
         {/* Центральная кнопка */}
         <div className="absolute -top-12 left-1/2 -translate-x-1/2">
-          <button className="w-20 h-20 bg-[#6B9960] rounded-2xl rotate-45 flex items-center justify-center group transition-transform hover:scale-105">
+          <button style={{width: "70px", height: "70px", border: "solid 8px #fff"}} className="bg-[#6B9960] rounded-3xl transparent-img rotate-45 flex items-center justify-center group transition-transform hover:scale-105">
             <div className="-rotate-45">
               <Flame size={36} className="text-white" />
             </div>
